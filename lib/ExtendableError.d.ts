86f2ccad4d3dab686f4f36704f94f4a8d4f631b3
Mike <michael_eric_moore@hotmail.com>
2020-01-23 22:17:58 -0800

    updated typings

--- lib/ExtendableError.d.ts
+++ lib/ExtendableError.d.ts
@@ -1,9 +1,9 @@
 declare class ExtendableError extends Error {
-  public static setUnenumerable<T>(obj: T, key: string, value: any): T;
-  public static makeUnenumerable<T>(obj: T, key: string): T;
+  public static setUnenumerable<T>(obj: T, key: string | number | symbol, value: any): T;
+  public static makeUnenumerable<T>(obj: T, key: string | number | symbol): T;
   
-  public setUnenumerable(key: string, value: any): this;
-  public makeUnenumerable(key: string): this;
+  public setUnenumerable(key: string | number | symbol, value: any): this;
+  public makeUnenumerable(key: string | number | symbol): this;
 }
 
 export = ExtendableError;
